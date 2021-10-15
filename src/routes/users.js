9f2af978736299e26be60960c34c52b7895c75ce
DavidSia <davidmiitarashi@gmail.com>
2021-10-15 19:12:32 -0300

    feat: add endpoint to list users

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ import UserController from '../app/controllers/UserController';
 
 const users = (routes) => {
   routes.get('/', UserController.index);
+  routes.get('/users', UserController.indexUser);
 };
 
 module.exports = users;
