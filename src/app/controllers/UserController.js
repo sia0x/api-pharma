921cf56e092b145cb760a0172fbfa5fea2fee852
DavidSia <davidmiitarashi@gmail.com>
2021-10-15 19:08:51 -0300

    feat: add list users method to controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,18 @@
+import List from '../repositories/User/list';
 class UserController {
   async index(req, res) {
     return res
       .status(200)
       .json({ message: 'REST Back-end Challenge 20201209 Running' });
+  }
+
+  async indexUser(req, res) {
+    try {
+      const users = await List();
+      return res.status(200).json(users);
+    } catch (error) {
+      return res.status(500).json('Internal server error');
+    }
   }
 }
 
