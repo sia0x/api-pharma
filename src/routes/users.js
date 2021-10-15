a0bb6f925c9c952d389aa98a3ae332e6444e5a77
DavidSia <davidmiitarashi@gmail.com>
2021-10-15 20:42:57 -0300

    feat: add endpoint to delete user

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,7 @@ const users = (routes) => {
   routes.get('/', UserController.index);
   routes.get('/users', UserController.indexUser);
   routes.get('/users/:id', UserController.findById);
+  routes.delete('/users/:id', UserController.delete);
 };
 
 module.exports = users;
