fa5f3f775c1447ec78f0f5cd6f57671368b4dda5
DavidSia <davidmiitarashi@gmail.com>
2021-10-15 19:45:48 -0300

    refactor: sort users by most recent

--- src/app/repositories/User/list.js
+++ src/app/repositories/User/list.js
@@ -1,7 +1,7 @@
 import User from '../../models/User';
 
 const list = async () => {
-  let users = await User.find();
+  let users = await User.find().sort({ imported_t: -1 });
 
   return users;
 };
