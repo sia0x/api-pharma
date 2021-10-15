8deafe09f163d2a4a71809aa9a6aa90cc8c2d901
DavidSia <davidmiitarashi@gmail.com>
2021-10-15 19:46:18 -0300

    feat: add endpoint to search user by id

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,7 @@ import UserController from '../app/controllers/UserController';
 const users = (routes) => {
   routes.get('/', UserController.index);
   routes.get('/users', UserController.indexUser);
+  routes.get('/users/:id', UserController.findById);
 };
 
 module.exports = users;
