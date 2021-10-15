6849ba9ee4bfc51aa81b2f31536ea11d8dd4023c
DavidSia <davidmiitarashi@gmail.com>
2021-10-15 20:27:35 -0300

    feat: validate mongodb id

--- src/app/repositories/User/findById.js
+++ src/app/repositories/User/findById.js
@@ -1,9 +1,12 @@
+import mongoose from 'mongoose';
 import User from '../../models/User';
 
 const list = async (data) => {
   const { id } = data;
 
-  let user = await User.findById(id);
+  const isValid = mongoose.Types.ObjectId.isValid(id);
+
+  if (!isValid) return { message: 'Necessário informar um id válido' };
 
   if (!user) return {};
 
