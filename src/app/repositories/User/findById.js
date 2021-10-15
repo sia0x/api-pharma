8c892a190610aced4674d04c2b1ef40af104e5de
DavidSia <davidmiitarashi@gmail.com>
2021-10-15 20:29:55 -0300

    refactor: change method name

--- src/app/repositories/User/findById.js
+++ src/app/repositories/User/findById.js
@@ -1,16 +1,18 @@
 import mongoose from 'mongoose';
 import User from '../../models/User';
 
-const list = async (data) => {
+const findById = async (data) => {
   const { id } = data;
 
   const isValid = mongoose.Types.ObjectId.isValid(id);
 
   if (!isValid) return { message: 'Necessário informar um id válido' };
 
+  const user = await User.findById(id);
+
   if (!user) return {};
 
   return user;
 };
 
-export default list;
+export default findById;
