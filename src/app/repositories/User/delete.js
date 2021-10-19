7ebd8436bc0d5ceeaab712d270c678620b403a54
DavidSia <davidmiitarashi@gmail.com>
2021-10-19 11:33:18 -0300

    feat: add validation error to delete repository

--- src/app/repositories/User/delete.js
+++ src/app/repositories/User/delete.js
@@ -1,16 +1,26 @@
 import mongoose from 'mongoose';
 import User from '../../models/User';
+import ExceptionStatus from '../../utils/exceptionStatus';
+import ExceptionValidation from '../../utils/exceptionValidation';
 
 const remove = async (data) => {
   const { id } = data;
 
   const isValid = mongoose.Types.ObjectId.isValid(id);
 
-  if (!isValid) return { message: 'Necessário informar um id válido!' };
+  if (!isValid)
+    throw new ExceptionValidation({
+      message: 'Necessário informar um id válido!',
+      fieldWithError: id,
+    });
 
   const user = await User.findByIdAndDelete(id);
 
-  if (!user) return { message: 'Usuário não foi encontrado!' };
+  if (!user)
+    throw new ExceptionStatus({
+      message: 'Usuário não foi encontrado!',
+      status: 404,
+    });
 
   return { message: 'Usuário deletado com sucesso!' };
 };
