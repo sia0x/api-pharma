c3b356ce0c0d894c521ea0210d7ec4c1b839b9e7
DavidSia <davidmiitarashi@gmail.com>
2021-10-19 17:18:02 -0300

    feat: add pagination to list users repository

--- src/app/repositories/User/list.js
+++ src/app/repositories/User/list.js
@@ -1,7 +1,12 @@
 import User from '../../models/User';
 
-const list = async () => {
-  let users = await User.find().sort({ imported_t: -1 });
+const list = async (params) => {
+  const { page = 1, limit = 10 } = params;
+
+  let users = await User.find()
+    .skip(+page * +limit)
+    .limit(+limit)
+    .sort({ imported_t: -1 });
 
   return users;
 };
