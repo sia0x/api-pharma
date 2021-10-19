7b002d7904233679a44faed8aa323550e09bfb96
DavidSia <davidmiitarashi@gmail.com>
2021-10-19 17:16:10 -0300

    feat: add pagination to list users in controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -11,7 +11,7 @@ class UserController {
 
   async indexUser(req, res) {
     try {
-      const users = await List();
+      const users = await List(req.query);
       return res.status(200).json(users);
     } catch (error) {
       return res.status(500).json('Internal server error');
